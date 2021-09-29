f5d8868d8365001f387613a68a822c6a5d6e589d
Michael <moskvichevmihail60@yandex.ru>
2021-09-29 22:43:57 +0300

    Исправление замечаний

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  if (min > 0 && max > 0) {
+    if (min < max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    return Math.floor(Math.random() * (min - max + 1)) + max;
+  }
 }
 
 getRandomInt(1, 10);
 
 function checkStringLength(string, length) {
-  return (string.length === length);
+  return (string.length <= length);
 }
 
 checkStringLength('Проверяемая строка', 5);
