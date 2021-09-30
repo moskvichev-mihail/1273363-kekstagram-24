a833a535a21c67b05943c8277aa8fbd94434ced8
Michael <moskvichevmihail60@yandex.ru>
2021-10-01 00:12:45 +0300

    Ещё правки

--- js/main.js
+++ js/main.js
@@ -3,7 +3,12 @@ function getRandomInt(min, max) {
     if (min < max) {
       return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-    return Math.floor(Math.random() * (min - max + 1)) + max;
+    else {
+      return 'Значение Min должно быть меньше, чем Max.';
+    }
+  }
+  else {
+    return 'Значения Min и Max могут быть только положительными.';
   }
 }
 
